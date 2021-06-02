950658862a824a20b8671b9a9770983e2ac98542
Ash <ashley@juice49.com>
2021-06-02 21:32:02 +0100

    Allow Chrome executable path to be customised via `CHROME_EXECUTABLE_PATH` environment variable (closes #4)

--- src/lib/get-image.ts
+++ src/lib/get-image.ts
@@ -7,8 +7,9 @@ export default async function getImage(
 ): Promise<Buffer | String | void> {
   const browser = await puppeteer.launch({
     args: chrome.args,
-    executablePath: await chrome.executablePath,
     headless: chrome.headless,
+    executablePath:
+      process.env.CHROME_EXECUTABLE_PATH ?? (await chrome.executablePath),
   })
 
   const page = await browser.newPage()
